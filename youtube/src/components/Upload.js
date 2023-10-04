5e3861c1faebbe3267ced1cf1097f9f69ac42949
aman00009 <146929900+aman00009@users.noreply.github.com>
2023-10-04 15:58:08 +0530

    Update Upload.js

--- youtube/src/components/Upload.js
+++ youtube/src/components/Upload.js
@@ -112,7 +112,7 @@ const Upload = ({ setOpen }) => {
             const axiosForUpload = axios.create({
                 withCredentials: true,
             });
-            const res = await axiosForUpload.post("http://localhost:4004/api/videos", { ...inputs, tags })
+            const res = await axiosForUpload.post("https://youtube12.vercel.app/api/videos", { ...inputs, tags })
             setOpen(false)
             res.status === 200 && navigate(`/video/${res.data._id}`)
 
