781d8d00dbed91df7b380423dec5d17274b83a10
aman00009 <146929900+aman00009@users.noreply.github.com>
2023-10-05 11:07:05 +0530

    Update index.js

--- YT-Server/routes/index.js
+++ YT-Server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send(' hello index');
+  res.send(' hello index from server');
 });
 
 module.exports = router;
